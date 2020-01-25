81fb00fadf43d608ceb60e52c37dc5d1fe825fd1
Dmis <sv.l1@mail.ru>
2020-01-25 18:57:54 +0300

    fix build

--- cmake_ctests/bootstrap.sh
+++ cmake_ctests/bootstrap.sh
@@ -4,6 +4,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 mkdir -p "$DIR"/conan_deps
-(cd "$DIR"/conan_deps && conan install .. )
+(cd "$DIR"/conan_deps && conan install .. -s compiler.cppstd=14 -s compiler.libcxx=libstdc++11 --build=missing )
 
 cmake -DCMAKE_PROJECT_INCLUDE="$DIR"/conan_deps/conan_paths.cmake "$@"
